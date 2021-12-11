15b6eec42ca60698f4d4905111447ca83141349f
Luigivalente <91776312+Luigivalente@users.noreply.github.com>
2021-12-11 13:02:33 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
+#include "lib.h"
+
+float maggiore(float numeri[], int dim){
+    float max=numeri[0];
+    for(int i=1; i<dim; i++){
+        if(numeri[i]>max){
+            max=numeri[i];
+        }
+    }
+    return max;
+}
 
