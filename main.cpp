3123a2dbe9ddd10c2ed7bde0069505cbdadce4cc
Luigivalente <91776312+Luigivalente@users.noreply.github.com>
2021-12-11 13:03:16 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,1 @@
-#include <iostream>
-#include "lib.h"
-using namespace std;
 
-int main() {
-    int dim;
-    cin >> dim;
-    float numeri[dim];
-    for(int i=0; i<dim; i++){
-        numeri[i]=0;
-    }
-    for(int i=0; i<dim; i++){
-        cin >> numeri[i];
-    }
-    cout <<maggiore(numeri,dim)<<endl;
-    return 0;
-}
